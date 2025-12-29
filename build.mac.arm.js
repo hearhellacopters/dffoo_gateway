d03d1c956d2689e1193d4095cb2085c361206918
hearhellacopters <salpesce@gmail.com>
2025-12-28 23:54:36 -0500

    Updated android apks and build process

---
+++ build.mac.arm.js
@@ -0,0 +1,24 @@
+/**
+ * @file for creating an executable .exe file for windows.
+ */
+
+const exe = require("@hearhellacopters/exe");
+const pak = require('./package.json');
+
+const build = exe({
+  entry: "./app.js",
+  out: "./dffoo_ios_signer-macos-arm",
+  pkg: ["-C", "GZip"], // Specify extra pkg arguments
+  version: pak.version,
+  target: "node18-macos-arm64",
+  //icon: "./app.ico", // Application icons must be same size as prebuild target
+  executionLevel: "highestAvailable",
+  properties:{
+        FileDescription: pak.description,
+        ProductName: pak.name,
+        OriginalFilename: pak.name,
+        LegalCopyright: "MIT"
+    }
+});
+
+build.then(() => console.log("Mac Arm build completed!"));
